021f67eeb9fbc518f6870dc90b036e47378cac92
tadele <tadele.abere@gmail.com>
2026-06-18 18:31:55 +0300

    Add FEMIS data and fix settings

--- afe/settings.py
+++ afe/settings.py
@@ -108,12 +108,12 @@ WSGI_APPLICATION = "afe.wsgi.application"
 
 # Database
 if os.environ.get('DATABASE_URL'):
-    DATABASES = {
-        'default': dj_database_url.config(
-            default=os.environ.get('DATABASE_URL'),
-            conn_max_age=600
-        )
-    }
+   DATABASES = {
+    "default": dj_database_url.config(
+        default="sqlite:///db.sqlite3",
+        conn_max_age=600
+    )
+}
 else:
     DATABASES = {
         "default": {
--- build.sh
+++ build.sh
@@ -4,3 +4,11 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+python manage.py loaddata femis_data.json
+python manage.py shell -c "
+from core.models import AfeUser
+user = AfeUser.objects.get(username='femis_admin')
+user.set_password('Admin@123')
+user.save()
+print('Password reset done')
+"
