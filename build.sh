4ef6cb4953171ce1141991e7e76148008d1f1efc
tadele <tadele.abere@gmail.com>
2026-06-18 18:05:47 +0300

    Reset femis_admin password

--- build.sh
+++ build.sh
@@ -4,3 +4,11 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+python manage.py loaddata userdata.json
+python manage.py shell -c "
+from core.models import AfeUser
+user = AfeUser.objects.get(username='femis_admin')
+user.set_password('admin@123')
+user.save()
+print('Password reset done')
+"
